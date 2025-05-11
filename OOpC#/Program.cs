b91edd26b856f28fd08c0438797a99c7b1edc8fb
MaNnu81 <mannuspamm@gmail.com>
2025-05-11 15:08:05 +0200

    versione 1 + versione 2

--- OOpC#/Program.cs
+++ OOpC#/Program.cs
@@ -7,36 +7,6 @@ namespace OOpC_
 {
     internal class Program
     {
-
-
-        private static void EseguiOperazioni(BankAccount account)
-        {
-            while (true) // Loop infinito (fino a exit)
-            {
-                Console.WriteLine("\nOperazioni disponibili:");
-                Console.WriteLine("1 - Deposito/Prelievo");
-                Console.WriteLine("2 - Visualizza saldo");
-                Console.WriteLine("3 - Esci");
-                Console.Write("Scelta: ");
-
-                var scelta = Console.ReadLine();
-
-                switch (scelta)
-                {
-                    case "1":
-                        account.BankingMovement();
-                        break;
-                    case "2":
-                        Console.WriteLine($"\nSaldo attuale: {account.Balance:C}");
-                        break;
-                    case "3":
-                        return; // Esce dal metodo
-                    default:
-                        Console.WriteLine("Scelta non valida!");
-                        break;
-                }
-            }
-        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -118,3 +88,102 @@ namespace OOpC_
         }
     }
 }
+
+
+
+
+
+//////////////////////////////////////////////////
+///////////////////////VERSIONE 2.0//////////////////////////
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Transactions;
+
+//namespace OOpC_
+//{
+//    internal class Program
+//    {
+//        private static void EseguiOperazioni(BankAccount account)
+//        {
+//            while (true)
+//            {
+//                Console.WriteLine("\nOperazioni disponibili:");
+//                Console.WriteLine("1 - Deposito/Prelievo");
+//                Console.WriteLine("2 - Visualizza saldo");
+//                Console.WriteLine("3 - Visualizza storico");
+//                Console.WriteLine("4 - Esci");
+//                Console.Write("Scelta: ");
+
+//                var scelta = Console.ReadLine();
+
+//                switch (scelta)
+//                {
+//                    case "1":
+//                        Console.Clear();
+//                        account.BankingMovement();
+//                        break;
+//                    case "2":
+//                        Console.Clear();
+//                        Console.WriteLine($"\nSaldo attuale: {account.Balance:C}");
+//                        break;
+//                    case "3":
+//                        Console.Clear();
+//                        Console.WriteLine(account.ToString());
+//                        break;
+//                    case "4":
+//                        return;
+//                    default:
+//                        Console.WriteLine("Scelta non valida!");
+//                        break;
+//                }
+//            }
+//        }
+
+//        static void Main(string[] args)
+//        {
+//            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//            // Inizializzazione dati
+//            DateTime dob1 = new DateTime(2000, 12, 3);
+//            var c1 = new Customer("Pippo", "Depippis", dob1, "via Gramsci, Genova 16123, Italy", "pippothereal@topoliniamail.to");
+//            var c2 = new Customer("Clarabella", "ManonBellissima", 2001, 05, 06, "via Vannucci, Genova 16123, Italy", "Cl4raBella@topoliniamail.to");
+//            var c3 = new Customer("Orazio", "Pancrazio", new DateTime(1984, 3, 20), "via Vannucci, Genova 16123, Italy", "Cl4raBella@topoliniamail.to");
+
+//            var e1 = new Employee("Paperino", "Paperino", 1980, 3, 25, "Topolinia1");
+//            var v1 = new VipCustomer("Topolino", "Mouse", 1990, 1, 25, "via Cantore, Genova 16123, Italy", "topo_lino@topoliniamail.to", 1555);
+
+//            // Creazione account con saldo iniziale
+//            var ab1 = new BankAccount(c1, e1, 2022, 02, 20, new List<Transaction>(), 2500);
+//            var ab2 = new CashBackAb(c2, e1, 2018, 04, 20, new List<Transaction>(), 200);
+//            var ab3 = new SaveAb(c3, e1, 2018, 06, 21, new List<Transaction>(), 1000);
+
+//            // Modifica i costruttori delle classi derivate per supportare initialBalance
+//            List<BankAccount> bankaccounts = new List<BankAccount> { ab1, ab2, ab3 };
+
+//            Console.WriteLine("=== BENVENUTO NEL SISTEMA BANCARIO ===");
+//            Console.WriteLine("Seleziona l'account da utilizzare:");
+
+//            for (int i = 0; i < bankaccounts.Count; i++)
+//            {
+//                Console.WriteLine($"{i + 1} - {bankaccounts[i].Customer.Name} {bankaccounts[i].Customer.Surname}");
+//            }
+
+//            Console.Write("Scelta: ");
+//            if (int.TryParse(Console.ReadLine(), out int sceltaAccount) && sceltaAccount > 0 && sceltaAccount <= bankaccounts.Count)
+//            {
+//                Console.Clear();
+//                Console.WriteLine($"Account selezionato: {bankaccounts[sceltaAccount - 1].Customer.Name}");
+//                EseguiOperazioni(bankaccounts[sceltaAccount - 1]);
+//            }
+//            else
+//            {
+//                Console.WriteLine("Selezione non valida!");
+//            }
+
+//            Console.WriteLine("\nProgramma terminato. Premere un tasto per uscire...");
+//            Console.ReadKey();
+//        }
+//    }
+//}
